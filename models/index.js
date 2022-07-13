fd4a39e9e343131b2861c5dc26b6d092bd127210
paulooliveira152012 <paulooliveira152012@gmail.com>
2022-07-13 17:08:28 -0400

    done with this branch

--- models/index.js
+++ models/index.js
@@ -1,4 +1,16 @@
 const User = require('./User');
 const Post = require('./Post')
 
+
+//define one-to-many relationship (one user can have many notes, but never one note belong to many users)
+User.hasMany(Post, {
+    foreignKey: 'user_id'
+  });
+
+Post.belongsTo(User, {
+    foreignKey: 'user_id',
+  });
+
+
+
 module.exports = { User, Post };
--- routes/api/index.js
+++ routes/api/index.js
@@ -2,7 +2,10 @@
 const router = require('express').Router();
 //require access to user-routes.js
 const userRoutes = require('./user-routes.js');
+//require access to post-routes.js
+const postRoutes = require('./post-routes')
 //use all routes
 router.use('/users', userRoutes);
+router.use('/posts', postRoutes);
 //export routes to where it's needed
 module.exports = router;
