b15ad39edc37ac74c09bfbad4b5904b25e3c23e9
paulooliveira152012 <paulooliveira152012@gmail.com>
2022-07-15 15:27:33 -0400

    adding refferences on Comments.js

--- models/Comment.js
+++ models/Comment.js
@@ -25,11 +25,17 @@ Comment.init(
     },
     user_id: {
         type: DataTypes.INTEGER,
-        allowNull: false
+        references: {
+            model: 'user',
+            key: 'id'
+        }
     },
     post_id: {
         type: DataTypes.INTEGER,
-        allowNull: false
+        references: {
+            model: 'post',
+            key: 'id'
+        }
     }
   },
   {
