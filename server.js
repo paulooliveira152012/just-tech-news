ec309d79b955229676d54d0727f644542e7a06a5
paulooliveira152012 <paulooliveira152012@gmail.com>
2022-07-18 18:47:12 -0400

    commit

--- server.js
+++ server.js
@@ -1,12 +1,26 @@
 const express = require('express');
-const routes = require('./routes');
+const routes = require('./controllers');
 const sequelize = require('./config/connection');
+const path = require('path');
 
 const app = express();
 const PORT = process.env.PORT || 3001;
 
+
+//setting up Handlebars as default 
+const exphbs = require('express-handlebars');
+const hbs = exphbs.create({});
+
+app.engine('handlebars', hbs.engine);
+app.set('view engine', 'handlebars');
+//------------------------------------
+
+
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+//express.static is a middleware function that takes all content from folder and serve them as static assets
+app.use(express.static(path.join(__dirname, 'public')));
+
 
 // turn on routes
 app.use(routes);
